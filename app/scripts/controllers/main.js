6ee6651cc72621c4324305a9de81232720d26530
abdel <abdeldw@gmail.com>
2013-06-17 02:38:37 -0600

    arreglos hehe, se puede cambiar el idioma de un dropdown

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,35 +2,77 @@
 
 angular.module('geoAngularApp').controller('MainCtrl',
   ['$scope','lenguagesSwitch', function ($scope,lenguagesSwitch){
-    $scope.countryCode = "US";
-    lenguagesSwitch.getLenguages($scope.countryCode).then(function(data){            
+    //set english as default language
+    $scope.countryCode = "US";//use ISO 3166-1-alpha-2 code
+    //call the service function and send the contry code as a parameter
+    //to load the correct json. 
+    lenguagesSwitch.getLanguages($scope.countryCode).then(function(data){    
+      //fill the $scope object the json data(languages)        
       $scope.languagesJSON = data[$scope.countryCode];            
     });
-    $scope.pageLenguages = [
-        "spañol",
-        "ingles",
-        "aleman"
+    //model dropdown languages menu.
+    $scope.pageLanguages = [
+      "Español",
+      "English",
+      "Italiano",
+      "Deutsch",
+      "French"
     ];
+    //this function change the language reciving a parameter from dropdown languages menu.
+    //ng-click(language)
+    $scope.changeLang = function(lenguage){
+      //evaluate what is the parameter value.
+      if(lenguage === 'Español'){
+        $scope.countryCode = "ES";
+        lenguagesSwitch.getLanguages($scope.countryCode).then(function(data){            
+          $scope.languagesJSON = data[$scope.countryCode];            
+        }); 
+      }else if(lenguage === 'English'){
+        $scope.countryCode = "US";
+        lenguagesSwitch.getLanguages($scope.countryCode).then(function(data){            
+          $scope.languagesJSON = data[$scope.countryCode];            
+        }); 
+      }else if(lenguage === 'Italiano'){
+        $scope.countryCode = "IT";
+        lenguagesSwitch.getLanguages($scope.countryCode).then(function(data){            
+          $scope.languagesJSON = data[$scope.countryCode];            
+        }); 
+      }else if(lenguage === 'Deutsch'){
+        $scope.countryCode = "DE";
+        lenguagesSwitch.getLanguages($scope.countryCode).then(function(data){            
+          $scope.languagesJSON = data[$scope.countryCode];            
+        }); 
+      }else if(lenguage === 'French'){
+        $scope.countryCode = "FR";
+        lenguagesSwitch.getLanguages($scope.countryCode).then(function(data){            
+          $scope.languagesJSON = data[$scope.countryCode];            
+        }); 
+      }
+    }
     //ask if the navigator has Geolocations
     if(navigator.geolocation) {
     //this method get the user's position and return an object
-    //with the cordinates in the especified funtion(paramater get location)
+    //with the cordinates in the especified function(paramater get location)
       navigator.geolocation.getCurrentPosition(getLocation);
     }else{
       $scope.valuei18n = 'Geolocation is not supported by this browser.';
     }
+    //this function get the user's location, recive the cords as a parameter.
     function getLocation(position){
+      //get the lat and long from position obj.
       var lat = position.coords.latitude 
          ,lng = position.coords.longitude 
-         ,mapResults = []
-         ,latlng = new google.maps.LatLng(lat,lng);
-      $scope.geocoder = new google.maps.Geocoder();  
+         ,mapResults = []//create an empty obj, will be fill up by the google map json obj.
+         ,latlng = new google.maps.LatLng(lat,lng);//get the lat and long.
+      $scope.geocoder = new google.maps.Geocoder();// instantiate a new google map object.
+      //send the lat and long to get the location info in a obj. 
       $scope.geocoder.geocode( {'latLng':latlng}, function(results, status){        
-        if (status == google.maps.GeocoderStatus.OK) {
-          mapResults = results[1].address_components;
+        if(status == google.maps.GeocoderStatus.OK){
+          mapResults = results[1].address_components;//fill the obj with the callback results
+          //get the al the object parameter "short_name" 
+          //and them get the last one that have the country code with lo-Dash.
           $scope.countryCode = _.last(_.pluck(mapResults, 'short_name'));
-          var lang = $scope.countryCode;
-          lenguagesSwitch.getLenguages($scope.countryCode, lang).then(function(data){            
+          lenguagesSwitch.getLanguages($scope.countryCode).then(function(data){            
             $scope.languagesJSON = data[$scope.countryCode];     
           });
         }else{
--- app/scripts/services/lenguagesSwitch.js
+++ app/scripts/services/lenguagesSwitch.js
@@ -3,7 +3,8 @@
 angular.module('geoAngularApp')
   .factory('lenguagesSwitch', ['$http','$q',function($http,$q){
     return{
-      getLenguages : function(lang){
+      //this function load the correct jason usin the country code.
+      getLanguages : function(lang){
         var deferred = $q.defer();
         $http.get('scripts/i18n/lang_'+lang+'.json').success(function(data){
           deferred.resolve(data);
